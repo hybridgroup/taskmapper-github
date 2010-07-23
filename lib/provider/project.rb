4cff6dafb12e22c5b2545992ef948e51c1aa204e
Juan Espinosa <juan@time4code.com>
2010-07-23 15:50:39 -0500

    Change search implementation, in case an hash arrives it means that is looking for an specific repository

--- lib/provider/project.rb
+++ lib/provider/project.rb
@@ -8,18 +8,14 @@ module TicketMaster::Provider
       API = Octopi::Repository
       # declare needed overloaded methods here
       
-      def self.find(*options)
-	first, attributes = options
-	if first.class == Hash
-	  self::API.find(first)
-	else
-	  super(first, attributes)
-	end
-      end
       
       def self.search(options = {}, limit = 100)
 	raise "Please supply arguments for search" if options.blank?
-	self::API.find_all(options)[0...limit]
+	if options.is_a? Hash
+	  self::API.find(options).to_a
+	else
+	  self::API.find_all(options)[0...limit]
+	end
       end
       
       # copy from this.copy(that) copies that into this
