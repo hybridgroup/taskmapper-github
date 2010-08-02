a0246b08d68e37cb8293fe82f9033e115f31c0cb
Juan Espinosa <juan@time4code.com>
2010-08-02 14:25:36 -0500

    Replace http for https in a method called secure

--- lib/github/github.rb
+++ lib/github/github.rb
@@ -9,15 +9,19 @@ module TicketMaster::Provider
       base_uri "http://github.com/api/v2/json"
       
       def self.find_all(repo, number)
-	      response = HTTParty.get("#{URI}/issues/comments/#{repo.username}/#{repo.name}/#{number}", :query => auth_params).parsed_response['comments']
+	      HTTParty.get("#{URI}/issues/comments/#{repo.username}/#{repo.name}/#{number}", :query => auth_params).parsed_response['comments']
       end
       
       def self.create(repo, number, comment)
-	      response = HTTParty.post("#{URI.gsub('http','https')}/issues/comment/#{repo.username}/#{repo.name}/#{number}", :query => auth_params, :body => {:comment => comment}).parsed_response['comment']
+	      HTTParty.post("#{secure}/issues/comment/#{repo.username}/#{repo.name}/#{number}", :query => auth_params, :body => {:comment => comment}).parsed_response['comment']
       end
       
       def self.auth_params
 	      {:login => Octopi::Api.api.login, :token => Octopi::Api.api.token}
+      end
+
+      def self.secure
+        URI.gsub('http','https')
       end
     end
   end
