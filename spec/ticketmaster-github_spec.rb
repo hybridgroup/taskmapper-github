fa662677fffadb7a0af2ba58fd3ff6092244007c
Juan Espinosa <juan@time4code.com>
2010-08-03 14:15:25 -0500

    Github provider spec

--- spec/ticketmaster-github_spec.rb
+++ spec/ticketmaster-github_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "TicketmasterGithub" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Ticketmaster::Provider::Github" do
+  it "Should be able to instantiate a new instance" do
+    @github =  TicketMaster.new(:Github, {:login => 'juanespinosa', :token => 'e4ddb6f2a920cacb50f8dbd89b850848'})
+    @github.should be_an_instance_of(TicketMaster)
+    @github.should be_a_kind_of(TicketMaster::Provider::Github)
   end
 end
