397803e1be2d8de54cac78fad28c85e95c86ea38
Juan Espinosa <juan@time4code.com>
2010-08-12 16:34:26 -0500

    Change specs

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,6 +5,7 @@ describe "Ticketmaster::Provider::Github::Comment" do
     @github =  TicketMaster.new(:github, {:login => 'juanespinosa', :token => 'asdfghk'})
     @klass = TicketMaster::Provider::Github::Comment
     @api = TicketMaster::Provider::Github::GithubComment
+    @repository = Factory.build(:repository)
   end
   
   before(:each) do
@@ -13,7 +14,7 @@ describe "Ticketmaster::Provider::Github::Comment" do
     @ticket =  TicketMaster::Provider::Github::Ticket.new issue
     @comment = Factory.build(:comment)
     @comments = [@comment]
-    Octopi::Repository.stub!(:find).and_return(@repository)
+    Octopi::Repository.stub!(:find_all).and_return([@repository])
   end
   
   it "should be able to load all comments" do
