f1b97246745552723b3c4d6ac40485161793d204
Rafael George <george.rafael@gmail.com>
2011-02-28 18:30:26 +0000

    add require for ticketmaser

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
-require 'ticketmaster'
-require 'ticketmaster-github'
 require 'spec'
 require 'factory_girl'
+require 'ticketmaster'
+require 'ticketmaster-github'
 
 Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each {|f| require f} 
   Spec::Runner.configure do |config|
