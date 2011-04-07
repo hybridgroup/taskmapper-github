1c0bc363daabaad1ba07238041282a26e5207dbb
Rafael George <george.rafael@gmail.com>
2011-04-07 19:07:57 -0400

    remove old comment api it's not needed

--- lib/ticketmaster-github.rb
+++ lib/ticketmaster-github.rb
@@ -1,10 +1,5 @@
 #require YOUR_PROVIDER_API
 require 'octokit'
-require 'httparty'
-
-%w{ github }.each do |f|
-  require File.dirname(__FILE__) + '/github/' + f + '.rb';
-end
 
 %w{ github ticket project comment }.each do |f|
   require File.dirname(__FILE__) + '/provider/' + f + '.rb';
