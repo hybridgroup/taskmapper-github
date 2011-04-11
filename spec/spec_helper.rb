232d27aaa7797a15de7411d459f3a965bb6fa038
Rafael George <george.rafael@gmail.com>
2011-04-11 18:32:19 -0400

    reorder require inclusion for fixing specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
+require 'ticketmaster'
 require 'active_support/core_ext/string'
 require 'spec'
 require 'factory_girl'
-require 'ticketmaster'
 require 'ticketmaster-github'
 
 Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each {|f| require f} 
