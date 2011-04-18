38f66086d02525b8f2f75a894795bfa149ec0787
Rafael George <george.rafael@gmail.com>
2011-04-18 19:11:47 +0000

    fix authentication problem and add password field to work properly

--- lib/provider/github.rb
+++ lib/provider/github.rb
@@ -16,18 +16,21 @@ module TicketMaster::Provider
     def authorize(auth = {})
       @authentication ||= TicketMaster::Authenticator.new(auth)
       auth = @authentication
+      login = auth.login || auth.username
       if auth.login.blank? and auth.username.blank?
         raise TicketMaster::Exception.new('Please provide at least a username')
-      elsif auth.token.blank? || auth.password.blank?
-        login = auth.login || auth.username
-        TicketMaster::Provider::Github.login = 
+      elsif auth.token.blank? && auth.password.blank?
+        TicketMaster::Provider::Github.login = login
         TicketMaster::Provider::Github.user_token = nil
         TicketMaster::Provider::Github.api = Octokit.client(:login => login)
-      else
-        token = auth.token || auth.password
-        TicketMaster::Provider::Github.login = auth.login || auth.username
+      elsif !auth.token.blank?
+        TicketMaster::Provider::Github.login = login
         TicketMaster::Provider::Github.user_token = auth.token
-        TicketMaster::Provider::Github.api = Octokit.client(:login => auth.login, :token => token)
+        TicketMaster::Provider::Github.api = Octokit.client(:login => login, :token => auth.token)
+      elsif !auth.password.blank?
+        TicketMaster::Provider::Github.login = login
+        TicketMaster::Provider::Github.user_token = auth.token
+        TicketMaster::Provider::Github.api = Octokit.client(:login => login, :password => auth.password)
       end
     end
 
