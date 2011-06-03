27f4a600dc2baaf8e97a9e608e830ce59d87571e
Rafael George <george.rafael@gmail.com>
2011-06-03 13:21:37 +0000

    added a rvmrc

--- spec/ticketmaster-github_spec.rb
+++ spec/ticketmaster-github_spec.rb
@@ -2,10 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Github" do
 
-  it "should be able to instantiate a new instance" do
+  before(:each) do 
     @github = TicketMaster.new(:github, {:login => 'juanespinosa', :token => 'asdfghk'})
+  end
+
+  it "should be able to instantiate a new instance" do
     @github.should be_an_instance_of(TicketMaster)
     @github.should be_a_kind_of(TicketMaster::Provider::Github)
+  end
+
+  it "should implement the valid method" do 
+    @github.valid?.should be_true
   end
 
   it "should show attempted authenticated if a token is present" do
