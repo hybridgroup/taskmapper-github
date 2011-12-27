767050ef36091b61f71a8ae7d30b2ad09c063d58
Rafael George <george.rafael@gmail.com>
2011-12-27 18:34:30 -0400

    properly set the path to vcr's fixtures in vcr_setup

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,7 +1,7 @@
 require 'vcr'
 
 VCR.config do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.stub_with :fakeweb
   c.default_cassette_options = { :record => :once }
 end
