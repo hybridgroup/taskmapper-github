bb3eabba5ea743e2ffb12a0885343e07e62a486a
Rafael George <george.rafael@gmail.com>
2012-02-17 19:40:15 -0400

    fixed failing specs for comments, removing vcr and using fakeweb directly

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -3,15 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Ticketmaster::Provider::Github::Comment" do
   
   before(:each) do
-    VCR.use_cassette('provider-for-comments') { @github = TicketMaster.new(:github, {:login => 'ticketmaster-user', :password => 'Tm123456'}) }
-    VCR.use_cassette('project-for-comments') { @project = @github.projects.first }
-    VCR.use_cassette('ticket-for-comments') { @ticket = @project.tickets.first }
+    @github = TicketMaster.new(:github, {:login => 'ticketmaster-user', :password => 'Tm123456'})
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/users/ticketmaster-user/repos', 'projects.json')
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo/issues','issues.json')
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo/issues?state=closed','closed_issues.json')
+    @project = @github.projects.first
+    @ticket = @project.tickets.first
     @klass = TicketMaster::Provider::Github::Comment
     @api = Octokit::Client
   end
   
   it "should be able to load all comments" do
-    VCR.use_cassette('comments') { @comments = @ticket.comments } 
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo/issues/1/comments', 'comments.json')
+    @comments = @ticket.comments
     @comments.should be_an_instance_of(Array)
     @comments.first.should be_an_instance_of(@klass)
   end
