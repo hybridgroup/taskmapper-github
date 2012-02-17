2de0f2d51122b95abd0d4652ae53823c099d572f
Rafael George <george.rafael@gmail.com>
2012-02-17 19:21:45 -0400

    solved all failing specs for tickets * removed vcr because of malformed response and problem with multi_json

--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -2,43 +2,38 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ticketmaster::Provider::Github::Ticket" do
   before(:each) do
-    VCR.use_cassette('tickets-instance')  { @github = TicketMaster.new(:github, {:login => 'ticketmaster-user', :password => 'Tm123456'}) }
-    VCR.use_cassette('github-public-project-jquery') { @project = @github.project('tmtest-repo') }
+    @github = TicketMaster.new(:github, {:login => 'ticketmaster-user', :password => 'Tm123456'})
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo', 'project.json')
+    @project = @github.project('tmtest-repo')
     @ticket_id = 1
     @klass = TicketMaster::Provider::Github::Ticket
   end
-  
+
   it "should be able to load all tickets" do
-    VCR.use_cassette('project-tickets') { @tickets = @project.tickets }
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo/issues', 'issues.json')
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo/issues?state=closed', 'closed_issues.json')
+    @tickets = @project.tickets
     @tickets.should be_an_instance_of(Array)
     @tickets.first.should be_an_instance_of(@klass)
   end
 
   it "should be able to load tickets from an array of ids" do
-    VCR.use_cassette('github-tickets-by-array-id') { @tickets = @project.tickets([@ticket_id]) }
+    stub_get('https://ticketmaster-user:Tm123456@github.com/api/v2/json/issues/show/ticketmaster-user/tmtest-repo/1', 'issues/1.json')
+    @tickets = @project.tickets([@ticket_id])
     @tickets.should be_an_instance_of(Array)
     @tickets.first.should be_an_instance_of(@klass)
   end
 
   it "should be able to find tickets based on attributes" do
-    VCR.use_cassette('github-tickets-attributes') { @tickets = @project.tickets(:id => @ticket_id) }
+    @tickets = @project.tickets(:id => @ticket_id)
     @tickets.should be_an_instance_of(Array)
     @tickets.first.should be_an_instance_of(@klass)
   end
 
   it "should find a ticket by id(number)" do
-    VCR.use_cassette('github-by-id') { @ticket = @project.ticket(@ticket_id) }
+    @ticket = @project.ticket(@ticket_id)
     @ticket.should be_an_instance_of(@klass)
     @ticket.title.should be_eql('for testing')
   end
 
-# it "should return author in ticket" 
-#
-# it "should be able to open a new ticket" 
-#
-# it "should be able to update a existing ticket" 
-#
-# it "should be able to reopen a ticket" 
-#
-# it "should be able to close a ticket" 
 end
