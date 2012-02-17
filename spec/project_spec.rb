d654d7adeba108fd6e34635d98402fe178f430fe
Rafael George <george.rafael@gmail.com>
2012-02-17 19:11:21 -0400

    fixed all projects failing specs in 1.8.7 * vcr was generating bad data, went back to use direct fakeweb

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -20,30 +20,27 @@ describe "Ticketmaster::Provider::Github::Project" do
   end
 
   it "should be able to load all projects based on an array of name(id)" do 
-    pending
-    VCR.use_cassette('github-projects-by-id') { @projects = @github.projects([@repo_name]) }
+    stub_get('https://ticketmaster-user:Tm123456@api.github.com/repos/ticketmaster-user/tmtest-repo', 'project.json')
+    @projects = @github.projects([@repo_name])
     @projects.should be_an_instance_of(Array)
     @projects.first.should be_an_instance_of(@klass)
     @projects.first.id.should == @returned_repo
   end
 
   it "should be able to load a single project based on a single name(id)" do 
-    pending
-    VCR.use_cassette('github-project-by-id') { @project = @github.projects(@repo_name) }
+    @project = @github.projects(@repo_name)
     @project.should be_an_instance_of(@klass)
     @project.id.should == @returned_repo
   end
 
   it "should be able to find by name(id)" do
-    pending
-    VCR.use_cassette('github-project-by-name') { @p = @github.project(@repo_name) }
-    @p.should be_an_instance_of(@klass)
-    @p.id.should == @returned_repo
+    @project = @github.project(@repo_name)
+    @project.should be_an_instance_of(@klass)
+    @project.id.should == @returned_repo
   end
 
   it "should be able to find by attributes" do
-    pending
-    VCR.use_cassette('github-project-find-attributes') { @projects = @github.projects(:name => 'tmtest-repo') }
+    @projects = @github.projects(:name => 'tmtest-repo')
     @projects.should be_an_instance_of(Array)
     @projects.first.id.should be_eql(@returned_repo)
   end
